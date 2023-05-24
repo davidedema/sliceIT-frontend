bf88b0e3e15e16b271d7184c9b3503ac7713a542
davide <demartini.davide01@gmail.com>
2023-05-24 20:24:55 +0200

    Fixato problemi e connesso a backend

--- src/components/NewOut.vue
+++ src/components/NewOut.vue
@@ -20,31 +20,37 @@
                     <v-container>
                         <v-row>
                             <v-col cols="12" sm="6" md="4">
-                                <v-text-field label="Nome spesa*" required></v-text-field>
+                                <v-text-field label="Nome spesa*" v-model="nome" required></v-text-field>
                             </v-col>
                             <v-col cols="12" sm="6" md="4">
-                                <v-autocomplete label="Pagata da*" :items="['Mario', 'Bepi', 'Pino', 'Gigi']"
-                                    required></v-autocomplete>
+                                <v-autocomplete label="Pagata da*" v-model="pagataDa"
+                                    :items="partecipanti.map(item => item.nickname)" required></v-autocomplete>
                             </v-col>
                             <v-col cols="12" sm="6" md="4">
-                                <v-text-field label="Importo*" hint="Inserire importo in euro" required></v-text-field>
+                                <v-text-field label="Importo*" v-model="importo" hint="Inserire importo in euro"
+                                    required></v-text-field>
                             </v-col>
                             <v-col cols="12">
-                                <v-text-field label="Descrizione" hint="Massimo 100 caratteri" required></v-text-field>
+                                <v-text-field label="Descrizione" v-model="descrizione" hint="Massimo 100 caratteri"
+                                    required></v-text-field>
                             </v-col>
                             <v-col cols="12" sm="6" md="6">
-                                <v-autocomplete label="Partecipanti*" v-model="fields.selected" :items="fields.participants" multiple required></v-autocomplete>
+                                <!-- <v-autocomplete label="Partecipanti*" v-model="selected" :items="partecipanti.map(item => item.nickname)" multiple required></v-autocomplete> -->
+                                <v-autocomplete label="Partecipanti*" :items="partecipanti.map(item => item.nickname)" v-model="aux" @update:model-value="updateSelected"
+                                    multiple required></v-autocomplete>
+
                             </v-col>
                             <v-col cols="12" sm="6" md="6">
-                                <component v-for="select in fields.selected" :key="select" :is="select">
+                                <component v-for="(select, index) in selected" :key="index">
                                     <v-row>
                                         <v-col cols="12" sm="6" md="6">
                                             <!-- NOME PARTECIPANE -->
-                                            <v-card auto :title="select"></v-card>
+                                            <v-card auto :title="select.user"></v-card>
                                         </v-col>
                                         <v-col cols="12" sm="6" md="6">
                                             <!-- IMPORTO DA PAGARE -->
-                                            <v-text-field v-model="fields" label="Importo*" :is="select" hint="Importo dovuto" required></v-text-field>
+                                            <v-text-field v-model="select.value" label="Importo*" hint="Importo dovuto"
+                                                required></v-text-field>
                                         </v-col>
                                     </v-row>
                                 </component>
@@ -52,11 +58,11 @@
                         </v-row>
                         <v-row>
                             <v-col cols="12" sm="6" md="6">
-                                <v-text-field label="Periodicità*" hint="Inserire ogni quanto si ripete"
+                                <v-text-field v-model="periodo" label="Periodicità*" hint="Inserire ogni quanto si ripete"
                                     :disabled="!isPeriodic" required></v-text-field>
                             </v-col>
                             <v-col cols="12" sm="6" md="6">
-                                <v-switch label="È periodica" @click="setPeriodic">
+                                <v-switch v-model="isPeriodic" label="È periodica" @click="setPeriodic">
                                 </v-switch>
                             </v-col>
                         </v-row>
@@ -68,7 +74,7 @@
                     <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
                         Chiudi
                     </v-btn>
-                    <v-btn color="blue-darken-1" variant="text" @click="logSelected">
+                    <v-btn color="blue-darken-1" variant="text" @click="submitForm">
                         Crea
                     </v-btn>
                 </v-card-actions>
@@ -85,49 +91,18 @@ const userStore = useUserStore()
 export default {
     data: () => ({
         dialog: false,
-        isPeriodic: false,
-        users: ["Luca", "Franco", "Giacomo", "Mirco"],
+        nome: '',
+        pagataDa: '',
+        importo: '',
+        descrizione: '',
+        partecipanti: [{
+            id: '',
+            nickname: '',
+        }],
         selected: [],
-        debts: [],
-        fields: {
-            name: {
-                label: 'Nome spesa',
-                sortable: true,
-                fù
-            },
-            paidBy: {
-                label: 'Pagata da',
-                sortable: true
-            },
-            amount: {
-                label: 'Importo',
-                sortable: true
-            },
-            description: {
-                label: 'Descrizione',
-                sortable: true
-            },
-            participants: [{
-                user: {
-                    label: 'Partecipanti',
-                    sortable: true
-                },
-            }],
-            selected: [{
-                select: {
-                    label: 'Partecipanti',
-                    sortable: true
-                },
-                debt:{
-                    label: 'Importo',
-                    sortable: true
-                }
-            }],
-            periodicity: {
-                label: 'Periodicità',
-                sortable: true
-            }
-        }
+        aux: [],
+        isPeriodic: false,
+        periodo: '',
     }),
     methods: {
         setPeriodic() {
@@ -145,9 +120,15 @@ export default {
                 })
                 if (response.ok) {
                     const data = await response.json()
-                    this.users = data.members
-                    for(let i=0; i<this.users.length; i++){
-                        let response = await fetch('http://localhost:3001/api/v1/users/' + this.users[i] + '/', {
+                    this.partecipanti = data.members.map(member => {
+                        return {
+                            id: member,
+                            nickname: '',
+                        }
+                    })
+                    for (let i = 0; i < data.members.length; i++) {
+
+                        let response = await fetch('http://localhost:3001/api/v1/users/' + this.partecipanti[i].id + '/', {
                             method: 'GET',
                             headers: {
                                 'x-auth-token': userStore.token,
@@ -155,7 +136,7 @@ export default {
                         })
                         if (response.ok) {
                             const data = await response.json()
-                            this.fields.participants[i] = data.nickname
+                            this.partecipanti[i].nickname = data.nickname
                         }
                     }
                 }
@@ -163,11 +144,51 @@ export default {
                 console.log(error)
             }
         },
-        logSelected() {
-            for (let i = 0; i < this.fields.selected.length; i++) {
-                console.log(this.fields.selected[i].select + " " + this.fields.selected[i].debt)
+        submitForm(){
+            // trasformo i nickname in id
+            for(let i = 0; i < this.selected.length; i++){
+                if(this.partecipanti[i].nickname == this.selected[i].user){
+                    this.selected[i].user = this.partecipanti[i].id
+                }
+            }
+            // dati da inviare
+            const data = {
+                name: this.nome,
+                description: this.descrizione,
+                value: this.importo,
+                paidBy: this.pagataDa,
+                users: this.selected,
+                group: userStore.currentGroup,
+                periodic: {
+                    isPeriodic: this.isPeriodic,
+                    days: this.periodo,
+                },
+                tag: null,
+            }
+            try {                
+                const response = fetch('http://localhost:3001/api/v1/outgoings/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-auth-token': userStore.token,
+                    },
+                    body: JSON.stringify(data),
+                })
+                if(response.ok){
+                    this.dialog = false
+                }
+            } catch (error) {
+                console.log(error)
             }
         },
+        updateSelected() {
+            this.selected = this.aux.map(item => {
+                return {
+                    user: item,
+                    value: '',
+                }
+            })
+        }
     }
 }
 

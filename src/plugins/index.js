ab752f75616fe9879c5026a5465df47cb9cd238f
davide <demartini.davide01@gmail.com>
2023-05-29 15:53:51 +0200

    added persistent storage

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -9,8 +9,10 @@ import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import router from '../router'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 export function registerPlugins (app) {
   loadFonts()

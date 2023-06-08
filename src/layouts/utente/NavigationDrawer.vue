8514ae2345b3cfe5a34e193ae573c944f874231b
Davide De Martini <demartini.davide01@gmail.com>
2023-06-08 21:48:02 +0200

    added link to menu

--- src/layouts/utente/NavigationDrawer.vue
+++ src/layouts/utente/NavigationDrawer.vue
@@ -11,7 +11,7 @@
     >
       <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home" @click="goto('/home')"></v-list-item>
       <v-list-item prepend-icon="mdi-forum" title="Gruppi" value="about" @click="goto('/groups')"></v-list-item>
-      <v-list-item prepend-icon="mdi-account-group" title="Persone" value="group"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-group" title="Persone" value="group" @click="goto('/people')"></v-list-item>
       <v-list-item prepend-icon="mdi-account-details" title="Statistiche" value="stat"></v-list-item>
     </v-list>
     <template v-slot:append>

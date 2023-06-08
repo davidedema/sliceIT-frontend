c839e87692e0355ea97ba7fe27a9cc78c8788c52
rikistiz <riki.stiz06@gmail.com>
2023-06-08 18:22:57 +0200

    get user and group name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -58,12 +58,12 @@
                 <v-card elevation="0" v-for="item in creditors.creditors" :key="item.creditor">
                     <v-card-title>
                         <p class="text-h5 text-right">
-                            {{ item.creditor }}
+                            {{ getUserName(item.creditor) }}
                         </p>
                     </v-card-title>
                     <v-card-text v-for="i in item.value" :key="i.group">
                         <p class="text-body-1 text-right">
-                            dal gruppo {{ i.group }} $ {{ i.money}} 
+                            dal gruppo {{ getGroupName(i.group) }} $ {{ i.money}} 
                         </p>
                     </v-card-text>
                 </v-card>
@@ -75,12 +75,12 @@
                 <v-card elevation="0" v-for="item in debtors.debtors" :key="item.debtor">
                     <v-card-title>
                         <p class="text-h5 text-left">
-                            {{ item.debtors }}
+                            {{ getUserName(item.debtors) }}
                         </p>
                     </v-card-title>
                     <v-card-text v-for="i in item.value" :key="i.group">
                         <p class="text-body-1 text-left">
-                            dal gruppo {{ i.group }} $ {{ i.money}} 
+                            dal gruppo {{ getGroupName(i.group) }} $ {{ i.money}} 
                         </p>
                     </v-card-text>
                 </v-card>
@@ -98,6 +98,8 @@
     const API_URL = HOST + '/api/v1'
     const EDIT_URL = API_URL + '/users/' + userStore.id
     const HOME_URL = EDIT_URL + '/report'
+    const GROUP_URL = API_URL + '/groups'
+    const USER_URL = API_URL + '/users'
 
     export default {
         name: 'Report',
@@ -108,14 +110,68 @@
             return {
                 debtors: {},
                 creditors: {},
+                userNames: {
+                    id: [],
+                    name: []
+                },
+                groupNames: {
+                    id: [],
+                    name: []
+                }
             }
         },
         methods: {
-            getNameGroup (idGroup) {
-                for ( let i = 0; i < userStore.groups.length; i++ ) {
-                    if ( userStore.groups[i].id === idGroup ) {
-                        return userStore.groups[i].name
+            getUserName (userId) {
+                for (let i = 0; i < this.userNames.id.length; i++) {
+                    if (this.userNames.id[i] == userId) {
+                        //console.log(this.userNames.name[i])
+                        return this.userNames.name[i]
                     }
+                }
+            },
+            getGroupName (groupId) {
+                for (let i = 0; i < this.groupNames.id.length; i++) {
+                    if (this.groupNames.id[i] == groupId) {
+                        return this.groupNames.name[i]
+                    }
+                }
+            },
+            async fetchUserName (userId) {
+                try {
+                    const response = await fetch(USER_URL + "/" + userId , {
+                        method: 'GET',
+                        headers: {
+                            "x-auth-token": userStore.token,
+                        }
+                    });
+                    if (response.ok) {
+                        const data = await response.json()
+                        return data.nickname 
+                    } else {
+                        const errorData = await response.json();
+                        console.error("response failed:", errorData.message);
+                    }
+                } catch (error) {
+                    console.error("error: ", error);
+                }
+            },
+            async fetchGroupName (groupId) {
+                try {
+                    const response = await fetch(GROUP_URL + "/" + groupId , {
+                        method: 'GET',
+                        headers: {
+                            "x-auth-token": userStore.token,
+                        }
+                    });
+                    if (response.ok) {
+                        const data = await response.json()
+                        return data.name 
+                    } else {
+                        const errorData = await response.json();
+                        console.error("response failed:", errorData.message);
+                    }
+                } catch (error) {
+                    console.error("error: ", error);
                 }
             },
             async getReport () {
@@ -130,6 +186,43 @@
                         const data = await response.json()
                         this.debtors = data.debtors
                         this.creditors = data.creditors
+                        // get all the usernames of the debtors
+                        for (let i = 0; i < this.debtors.debtors.length; i++) {
+                            if(this.userNames.id.includes(this.debtors.debtors[i].debtors)) 
+                                continue
+                            this.userNames.id.push(this.debtors.debtors[i].debtors)
+                            let nameuser = await this.fetchUserName(this.debtors.debtors[i].debtors)
+                            this.userNames.name.push(nameuser)
+                        }
+                        // get all the usernames of the creditors
+                        for (let i = 0; i < this.creditors.creditors.length; i++) {
+                            if(this.userNames.id.includes(this.creditors.creditors[i].creditor)) 
+                                continue
+                            this.userNames.id.push(this.creditors.creditors[i].creditor)
+                            let nameuser = await this.fetchUserName(this.creditors.creditors[i].creditor)
+                            this.userNames.name.push(nameuser)
+                            console.log(this.userNames.id + " - " + this.userNames.name)
+                        }
+                        // get all the groupnames of the debtors
+                        for (let i = 0; i < this.debtors.debtors.length; i++) {
+                            for (let j = 0; j < this.debtors.debtors[i].value.length; j++) {
+                                if(this.groupNames.id.includes(this.debtors.debtors[i].value[j].group)) 
+                                    continue
+                                this.groupNames.id.push(this.debtors.debtors[i].value[j].group)
+                                let namegroup = await this.fetchGroupName(this.debtors.debtors[i].value[j].group)
+                                this.groupNames.name.push(namegroup)
+                            }
+                        }
+                        // get all the groupnames of the creditors
+                        for (let i = 0; i < this.creditors.creditors.length; i++) {
+                            for (let j = 0; j < this.creditors.creditors[i].value.length; j++) {
+                                if(this.groupNames.id.includes(this.creditors.creditors[i].value[j].group)) 
+                                    continue
+                                this.groupNames.id.push(this.creditors.creditors[i].value[j].group)
+                                let namegroup = await this.fetchGroupName(this.creditors.creditors[i].value[j].group)
+                                this.groupNames.name.push(namegroup)
+                            }
+                        }
                     } else {
                         const errorData = await response.json();
                         console.error("response failed:", errorData.message);
--- src/views/People.vue
+++ src/views/People.vue
@@ -2,18 +2,22 @@
     <p class="text-h2 text-left mt-5">
         Persone
     </p>
-    <div>
-
-        
-    </div>
-    <v-card width="1400" class="mx-auto mt-10" elevation="0">
+    
+    <v-card width="1000" class="mx-auto mt-10" elevation="0">
         <v-expansion-panels multiple variant="inset">
             <v-expansion-panel v-for="item in creditors.creditors" :key="item.creditor">
                 <v-expansion-panel-title expand-icon="mdi-plus" collapse-icon="mdi-minus">
-                    {{ item.creditor }}
+                    {{ getUserName(item.creditor) }}
                 </v-expansion-panel-title>
                 <v-expansion-panel-text v-for="i in item.value" :key="i.group">
-                    dal gruppo {{ i.group }} $ {{ i.money}} 
+                    <v-row>
+                        <v-col>
+                            {{ getGroupName(i.group) }}
+                        </v-col>
+                        <v-col class="text-right">
+                            $ {{ i.money}}
+                        </v-col>
+                    </v-row>
                 </v-expansion-panel-text>
             </v-expansion-panel>
 
@@ -21,10 +25,10 @@
 
             <v-expansion-panel v-for="item in debtors.debtors" :key="item.debtor">
                 <v-expansion-panel-title expand-icon="mdi-plus" collapse-icon="mdi-minus">
-                    {{ item.debtors }}
+                    {{ getUserName(item.debtors) }}
                 </v-expansion-panel-title>
                 <v-expansion-panel-text v-for="i in item.value" :key="i.group">
-                    dal gruppo {{ i.group }} $ {{ i.money}} 
+                    dal gruppo {{ getGroupName(i.group) }} $ {{ i.money}} 
                 </v-expansion-panel-text>
             </v-expansion-panel>
         </v-expansion-panels>
@@ -42,6 +46,8 @@
     const API_URL = HOST + '/api/v1'
     const EDIT_URL = API_URL + '/users/' + userStore.id
     const HOME_URL = EDIT_URL + '/report'
+    const GROUP_URL = API_URL + '/groups'
+    const USER_URL = API_URL + '/users'
 
     export default {
         name: 'People',
@@ -52,9 +58,79 @@
             return {
                 debtors: {},
                 creditors: {},
+                userNames: {
+                    id: [],
+                    name: []
+                },
+                groupNames: {
+                    id: [],
+                    name: []
+                }
             }
         },
         methods: {
+            orderDescending () {
+                
+            },
+            orderAscending () {
+
+            },
+            orderAlphabetical () {
+
+            },
+            getUserName (userId) {
+                for (let i = 0; i < this.userNames.id.length; i++) {
+                    if (this.userNames.id[i] == userId) {
+                        //console.log(this.userNames.name[i])
+                        return this.userNames.name[i]
+                    }
+                }
+            },
+            getGroupName (groupId) {
+                for (let i = 0; i < this.groupNames.id.length; i++) {
+                    if (this.groupNames.id[i] == groupId) {
+                        return this.groupNames.name[i]
+                    }
+                }
+            },
+            async fetchUserName (userId) {
+                try {
+                    const response = await fetch(USER_URL + "/" + userId , {
+                        method: 'GET',
+                        headers: {
+                            "x-auth-token": userStore.token,
+                        }
+                    });
+                    if (response.ok) {
+                        const data = await response.json()
+                        return data.nickname 
+                    } else {
+                        const errorData = await response.json();
+                        console.error("response failed:", errorData.message);
+                    }
+                } catch (error) {
+                    console.error("error: ", error);
+                }
+            },
+            async fetchGroupName (groupId) {
+                try {
+                    const response = await fetch(GROUP_URL + "/" + groupId , {
+                        method: 'GET',
+                        headers: {
+                            "x-auth-token": userStore.token,
+                        }
+                    });
+                    if (response.ok) {
+                        const data = await response.json()
+                        return data.name 
+                    } else {
+                        const errorData = await response.json();
+                        console.error("response failed:", errorData.message);
+                    }
+                } catch (error) {
+                    console.error("error: ", error);
+                }
+            },
             async getCreditorsDebtors () {
                 try {
                     const response = await fetch(HOME_URL, {
@@ -67,6 +143,43 @@
                         const data = await response.json()
                         this.debtors = data.debtors
                         this.creditors = data.creditors
+                        // get all the usernames of the debtors
+                        for (let i = 0; i < this.debtors.debtors.length; i++) {
+                            if(this.userNames.id.includes(this.debtors.debtors[i].debtors)) 
+                                continue
+                            this.userNames.id.push(this.debtors.debtors[i].debtors)
+                            let nameuser = await this.fetchUserName(this.debtors.debtors[i].debtors)
+                            this.userNames.name.push(nameuser)
+                        }
+                        // get all the usernames of the creditors
+                        for (let i = 0; i < this.creditors.creditors.length; i++) {
+                            if(this.userNames.id.includes(this.creditors.creditors[i].creditor)) 
+                                continue
+                            this.userNames.id.push(this.creditors.creditors[i].creditor)
+                            let nameuser = await this.fetchUserName(this.creditors.creditors[i].creditor)
+                            this.userNames.name.push(nameuser)
+                            console.log(this.userNames.id + " - " + this.userNames.name)
+                        }
+                        // get all the groupnames of the debtors
+                        for (let i = 0; i < this.debtors.debtors.length; i++) {
+                            for (let j = 0; j < this.debtors.debtors[i].value.length; j++) {
+                                if(this.groupNames.id.includes(this.debtors.debtors[i].value[j].group)) 
+                                    continue
+                                this.groupNames.id.push(this.debtors.debtors[i].value[j].group)
+                                let namegroup = await this.fetchGroupName(this.debtors.debtors[i].value[j].group)
+                                this.groupNames.name.push(namegroup)
+                            }
+                        }
+                        // get all the groupnames of the creditors
+                        for (let i = 0; i < this.creditors.creditors.length; i++) {
+                            for (let j = 0; j < this.creditors.creditors[i].value.length; j++) {
+                                if(this.groupNames.id.includes(this.creditors.creditors[i].value[j].group)) 
+                                    continue
+                                this.groupNames.id.push(this.creditors.creditors[i].value[j].group)
+                                let namegroup = await this.fetchGroupName(this.creditors.creditors[i].value[j].group)
+                                this.groupNames.name.push(namegroup)
+                            }
+                        }
                     } else {
                         const errorData = await response.json();
                         console.error("response failed:", errorData.message);
